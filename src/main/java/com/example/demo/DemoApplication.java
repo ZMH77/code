a49fb0d24a0ab39414b7dfbe1eefc38ed28e5866
zhangchuan@fenbi.com <zhangchuan@fenbi.com>
2020-06-02 16:14:39 +0800

    FIX version

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,7 +17,7 @@ public class DemoApplication {
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Helloo %s!", name);
+		return String.format("Helloo2 %s!", name);
 	}
 
 }
